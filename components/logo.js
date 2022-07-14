ab934e433d8983c82deb21160fa86e4ae644c2f3
tony.origogi <tony.origogi@kakaobrain.com>
2022-07-14 17:44:45 +0900

    Implemeting Logo Component

---
+++ components/logo.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { Text, useColorModeValue } from '@chakra-ui/react'
+
+import styled from '@emotion/styled'
+
+const LogoBox = styled.span`
+font-weight : bold;
+font-size : 18px;
+display : inline-flex;
+align-items : center;
+height : 30px;
+line-height : 20px
+padding : 10px
+
+&:hover img {
+    transform : rotate(20deg):
+}
+
+`
