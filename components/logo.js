c26ed61bc7bfcf5adbf82b49317e9d0a249c2867
tony.origogi <tony.origogi@kakaobrain.com>
2022-07-15 15:29:57 +0900

    Fix Logo bug

--- components/logo.js
+++ components/logo.js
@@ -5,20 +5,18 @@ import { Text, useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 
 const LogoBox = styled.span`
-  font-weight : bold;
-  font-size : 18px;
-  display : inline-flex;
-  align-items : center;
-  height : 30px;
-  line-height : 20px
-  padding : 10px
-
+  font-weight: bold;
+  font-size: 18px;
+  display: inline-flex;
+  align-items: center;
+  height: 30px;
+  line-height: 20px;
+  padding: 10px;
   img {
-    transition: 1000ms ease;
+    transition: 200ms ease;
   }
-
   &:hover img {
-      transform : rotate(100deg);
+    transform: rotate(20deg);
   }
 `
 
