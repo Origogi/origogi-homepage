c7a9452a49e53bae58096d12e63b575c1e15e5fb
tony.origogi <tony.origogi@kakaobrain.com>
2022-07-15 11:05:20 +0900

    Implemeted Logo Component

--- components/logo.js
+++ components/logo.js
@@ -5,16 +5,35 @@ import { Text, useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 
 const LogoBox = styled.span`
-font-weight : bold;
-font-size : 18px;
-display : inline-flex;
-align-items : center;
-height : 30px;
-line-height : 20px
-padding : 10px
+    font-weight : bold;
+    font-size : 18px;
+    display : inline-flex;
+    align-items : center;
+    height : 30px;
+    line-height : 20px
+    padding : 10px
 
-&:hover img {
-    transform : rotate(20deg):
+    &:hover img {
+        transform : rotate(20deg):
+    }
+`
+
+const Logo = () => {
+  const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
+
+  return (
+    <Link href="/">
+      <LogoBox>
+        <Image src={footPrintImg} width={20} height={20} alt="logo" />
+        <Text
+          color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+          fontFamily="M PLUS Rounded 1c"
+          fontWeight="bold"
+          ml={3}
+        />
+      </LogoBox>
+    </Link>
+  )
 }
 
-`
+export default Logo
