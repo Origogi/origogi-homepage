ce5a556a0c5b96bbcf6d9f82e7de5b70aba055da
tony.origogi <tony.origogi@kakaobrain.com>
2022-07-15 23:30:28 +0900

    Add theme js

---
+++ libs/theme.js
@@ -0,0 +1,26 @@
+import { extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+
+const styles = {
+  global: props => ({
+    body: {
+      bg: mode('#f0e7db', '#202023')(props)
+    }
+  })
+}
+const components = {
+  Heading: {
+    variants: {
+      'section-title': {
+        textDecoration: 'underline',
+        fontSize: 20,
+        textUnderlineOffset: 6,
+        textDecorationColor: '#525252',
+        textDecorationThickness: 4,
+        marginTop: 3,
+        marginBottom: 4
+      }
+    },
+    Link: {}
+  }
+}
