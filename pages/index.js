fd039e40c79b74f5213d88dfb52da328b3c143a5
Origogi <rlawjdxo1212@naver.com>
2022-07-16 13:21:18 +0900

    Add profile Image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello I&apos;m Mobile and Web developer based in Korea
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -12,6 +24,23 @@ const Page = () => {
             Jeongtae Kim (Origogi)
           </Heading>
           <p>Front-End Engineer ( Android / Flutter / Web )</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/origogi.png"
+            alt="Profile Image"
+          />
         </Box>
       </Box>
     </Container>
