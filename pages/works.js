7c6584434e27403aa639a051e27b6094ab271163
Origogi <rlawjdxo1212@naver.com>
2022-08-27 15:11:07 +0900

    Add works item

--- pages/works.js
+++ pages/works.js
@@ -1,5 +1,7 @@
 import { Container, Box, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+import thumbnail from '../public/images/works/thumbMovist.png'
 
 const Works = () => {
   return (
@@ -7,6 +9,19 @@ const Works = () => {
       <Heading as="h3" fontSize={20} mb={4}>
         Works
       </Heading>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+          <WorkGridItem id="movist" title="Movist" thumbnail={thumbnail}>
+            Show movie information through network (by Flutter)
+          </WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem id="movist" title="Movist" thumbnail={thumbnail}>
+            Show movie information through network (by Flutter)
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
     </Container>
   )
 }
