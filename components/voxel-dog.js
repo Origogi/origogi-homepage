8fedda8a9219190e88fb4ec348d9ccc944e11e06
Origogi <rlawjdxo1212@naver.com>
2022-08-29 01:54:46 +0900

    Fix canvas add twice

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -1,9 +1,8 @@
 import { useState, useEffect, useRef, useCallback } from 'react'
-import { Box, Spinner } from '@chakra-ui/react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { loadGLTFModel } from '../lib/model'
-import { Vector3 } from 'three'
+import { DogSpinner, DogContainer } from './voxel-dog-loader'
 
 function easeOutCirc(x) {
   return Math.sqrt(1 - Math.pow(x - 1, 4))
@@ -12,25 +11,23 @@ function easeOutCirc(x) {
 const VoxelDog = () => {
   const refContainer = useRef()
   const [loading, setLoading] = useState(true)
-  const [renderer, setRenderer] = useState()
-  const [_camera, setCamera] = useState()
-  const [target] = useState(new THREE.Vector3(-0.5, 1.2, 0))
-  const [initialCameraPosition] = useState(
-    new THREE.Vector3(
-      20 * Math.sin(0.2 * Math.PI),
-      10,
-      20 * Math.cos(0.2 * Math.PI)
-    )
-  )
+  const refRenderer = useRef()
 
-  const [scene] = useState(new THREE.Scene())
-  const [_controls, setControls] = useState()
+  const handleWindowResize = useCallback(() => {
+    const { current: renderer } = refRenderer
+    const { current: container } = refContainer
+    if (container && renderer) {
+      const scW = container.clientWidth
+      const scH = container.clientHeight
+
+      renderer.setSize(scW, scH)
+    }
+  }, [])
 
   /* eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
     const { current: container } = refContainer
-
-    if (container && !renderer) {
+    if (container) {
       const scW = container.clientWidth
       const scH = container.clientHeight
 
@@ -42,21 +39,29 @@ const VoxelDog = () => {
       renderer.setSize(scW, scH)
       renderer.outputEncoding = THREE.sRGBEncoding
       container.appendChild(renderer.domElement)
-      setRenderer(renderer)
+      refRenderer.current = renderer
+      const scene = new THREE.Scene()
+
+      const target = new THREE.Vector3(-0.5, 1.2, 0)
+      const initialCameraPosition = new THREE.Vector3(
+        20 * Math.sin(0.2 * Math.PI),
+        10,
+        20 * Math.cos(0.2 * Math.PI)
+      )
 
       // 640 -> 240
-      // 8 -> 6
+      // 8   -> 6
       const scale = scH * 0.005 + 4.8
       const camera = new THREE.OrthographicCamera(
         -scale,
         scale,
         scale,
+        -scale,
         0.01,
         50000
       )
       camera.position.copy(initialCameraPosition)
       camera.lookAt(target)
-      setCamera(camera)
 
       const ambientLight = new THREE.AmbientLight(0xcccccc, 1)
       scene.add(ambientLight)
@@ -64,12 +69,12 @@ const VoxelDog = () => {
       const controls = new OrbitControls(camera, renderer.domElement)
       controls.autoRotate = true
       controls.target = target
-      setControls(controls)
 
       loadGLTFModel(scene, '/dog.glb', {
         receiveShadow: false,
         castShadow: false,
       }).then(() => {
+        animate()
         setLoading(false)
       })
 
@@ -78,7 +83,7 @@ const VoxelDog = () => {
       const animate = () => {
         req = requestAnimationFrame(animate)
 
-        frame = frame < 100 ? frame + 1 : frame
+        frame = frame <= 100 ? frame + 1 : frame
 
         if (frame <= 100) {
           const p = initialCameraPosition
@@ -89,7 +94,6 @@ const VoxelDog = () => {
             p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed)
           camera.position.z =
             p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed)
-
           camera.lookAt(target)
         } else {
           controls.update()
@@ -100,33 +104,21 @@ const VoxelDog = () => {
 
       return () => {
         cancelAnimationFrame(req)
+        renderer.domElement.remove()
         renderer.dispose()
       }
     }
   }, [])
 
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowResize, false)
+    return () => {
+      window.removeEventListener('resize', handleWindowResize, false)
+    }
+  }, [handleWindowResize])
+
   return (
-    <Box
-      ref={refContainer}
-      className="voxel-dog"
-      m="auto"
-      at={['-20px', '-60px', '-120px']}
-      mb={['-40px', '-140px', '-200px']}
-      w={[280, 480, 640]}
-      h={[280, 480, 640]}
-      position="relative"
-    >
-      {loading && (
-        <Spinner
-          size="xl"
-          position="absolute"
-          left="50%"
-          top="50%"
-          ml="calc(0px - var(--spinner-size) / 2)"
-          mt="calc(0px - var(--spinner-size))"
-        />
-      )}
-    </Box>
+    <DogContainer ref={refContainer}>{loading && <DogSpinner />}</DogContainer>
   )
 }
 
