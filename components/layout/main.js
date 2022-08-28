97a41e1279debaf604ef55c4219e883d8910080d
Origogi <rlawjdxo1212@naver.com>
2022-08-28 15:25:19 +0900

    Add voxel dog

--- components/layout/main.js
+++ components/layout/main.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Box, Container } from '@chakra-ui/react'
 import Navbar from '../navbar'
+import VoxelDog from '../voxel-dog'
 
 const Main = ({ children, router }) => {
   return (
@@ -10,9 +11,10 @@ const Main = ({ children, router }) => {
         <title>Jeongtae Kim - Homepage</title>
       </Head>
 
-      <Navbar path={router.asPath}/>
+      <Navbar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
+        <VoxelDog />
         {children}
       </Container>
     </Box>
---
+++ components/voxel-dog.js
@@ -0,0 +1,17 @@
+import { useState, useEffect, useRef, useCallback } from 'react'
+import { Box, Spinner } from '@chakra-ui/react'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { loadGLTFModel } from '../lib/model'
+
+function easeOutCirc(x) {
+  return Math.sqrt(1 - Math.pow(x - 1, 4))
+}
+
+const VoxelDog = () => {
+  const refContainer = useRef()
+
+  return <Box ref={refContainer}>Dog!!</Box>
+}
+
+export default VoxelDog
